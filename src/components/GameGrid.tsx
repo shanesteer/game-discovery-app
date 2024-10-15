8144d71c62e49478a1806a327c48756e3456dbc7
Shane Ethan Steer <shaneethansteer@gmail.com>
2024-10-15 22:38:54 +0800

    Added thumbnail to genres and improve layout issue between game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,15 +6,15 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
